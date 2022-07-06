2e34568d61af03afc3b04bf04937d47f0f243f90
tatra-t <tanechka.dush@gmail.com>
2022-07-06 17:15:27 +0300

    search city and change temp

--- src/index.js
+++ src/index.js
@@ -76,3 +76,116 @@ function curPosition(position) {
 }
 navigator.geolocation.getCurrentPosition(curPosition);
 
+let curLoc = document.querySelector(".curLoc");
+curLoc.addEventListener("click", curPosition);
+
+
+function citySearch(event) {
+  event.preventDefault();
+  let cityName = document.querySelector(".me-2");
+  cityName = cityName.value
+    
+  function timeDate(response) {
+    let date = new Date(response.data.dt * 1000);
+    console.log(date)
+    let curHours = date.getHours();
+    if (curHours < 10) {
+      curHours = `0${curHours}`;
+    }
+    let curMinutes = date.getMinutes();
+    if (curMinutes < 10) {
+      curMinutes = `0${curMinutes}`;
+    }
+
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    
+    let curDay = days[date.getDay()];
+    console.log(curDay);
+    let curDate = date.getDate();
+    let curYear = date.getFullYear();
+    let month = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    let curMonth = month[date.getMonth()];
+    
+    let h1 = document.querySelector("h1");
+    h1.innerHTML = response.data.name; 
+    let dateNow = document.querySelector("#dateNow");
+    dateNow.innerHTML = `${curDate} ${curMonth} ${curYear}`;
+    let time = document.querySelector("#time");
+    time.innerHTML = `${curHours}:${curMinutes}`;
+    let weekDay = document.querySelector("#weekDay")
+    weekDay.innerHTML = curDay;
+    let tempCel = document.querySelector("#tempCel");
+    tempCel.innerHTML = Math.round(response.data.main.temp);
+    let emojiMain = document.querySelector("#emojiMain");
+    emojiMain.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    emojiMain.setAttribute("alt", response.data.weather[0].description);
+    let feelLike = document.querySelector("#feelLike");
+    feelLike.innerHTML = Math.round(response.data.main.feels_like);
+    console.log(response.data.main.feels_like)
+    let main = document.querySelector("#main");
+    main.innerHTML = response.data.weather[0].main;
+    let wind = document.querySelector("#wind")
+    wind.innerHTML = response.data.wind.speed;
+    let humidity = document.querySelector("#humidity");
+    humidity.innerHTML = response.data.main.humidity;
+  }
+
+  let apiKey = "5bd8dd5876af31be7dd1dd4666c7f2a5";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(timeDate);
+}
+let form = document.querySelector(".d-flex");
+form.addEventListener("submit", citySearch)
+
+
+function changeTemp(event) {
+    event.preventDefault();
+    let tempCel = document.querySelector("#tempCel").innerText;
+    let tempCel1 = document.querySelector("#tempCel");
+    let tempFahr = Math.round(tempCel * 1.8 + 32);
+    tempCel1.innerHTML = tempFahr;
+    let celsius = document.querySelector(".celsius");
+    let fahrenheit = document.querySelector(".fahrenheit");
+    celsius.innerHTML = `<span class="celsius"><a href="">C</a></span>`;
+    fahrenheit.innerHTML = `<span class="fahrenheit"> F </span>`;
+
+    function changeTempCel(event) {
+        event.preventDefault();
+        let tempFahr = document.querySelector("#tempCel").innerText;
+        let tempFahr1 = document.querySelector("#tempCel");
+        let tempCel = Math.round((tempFahr - 32) / 1.8);
+        tempFahr1.innerHTML = tempCel;
+        let celsius = document.querySelector(".celsius");
+        let fahrenheit = document.querySelector(".fahrenheit");
+        celsius.innerHTML = `<span class="celsius">C</span>`;
+      fahrenheit.innerHTML = `<span class="fahrenheit"><a href=""> F</a> </span>`;
+    }
+  
+    let cel = document.querySelector(".celsius");
+    cel.addEventListener("click", changeTempCel, false);
+}
+
+
+let fahr = document.querySelector(".fahrenheit");
+fahr.addEventListener("click", changeTemp);
